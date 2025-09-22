eae9ccf2d887d600bcd038d806b455afdc2d367e
healthykim <bsbs8645@snu.ac.kr>
2025-09-22 23:05:48 +0900

    add: implement blobfetcher

--- core/types/custody_bitmap.go
+++ core/types/custody_bitmap.go
@@ -10,13 +10,21 @@ import (
 // `CustodyBitmap` is a bitmap to represent which custody index to store (little endian)
 type CustodyBitmap [16]byte
 
+func NewCustodyBitmap(custody []uint64) CustodyBitmap {
+	result := CustodyBitmap{}
+	err := (&result).SetIndices(custody)
+	if err != nil {
+		panic("CustodyBitmap: bit index out of range")
+	}
+	return result
+}
 func (b CustodyBitmap) IsSet(i uint) bool {
 	if i >= uint(kzg4844.CellsPerBlob) {
 		return false
 	}
-	index := i / 8
-	offset := i % 8
-	return ((b[index] >> offset) & 1) == 1
+	byteIdx := i / 8
+	bitOff := i % 8
+	return ((b[byteIdx] >> bitOff) & 1) == 1
 }
 
 // Set ith bit
@@ -24,9 +32,9 @@ func (b *CustodyBitmap) Set(i uint) error {
 	if i >= uint(kzg4844.CellsPerBlob) {
 		return errors.New("bit index out of range")
 	}
-	index := i / 8
-	offset := i % 8
-	b[index] |= 1 << offset
+	byteIdx := i / 8
+	bitOff := i % 8
+	b[byteIdx] |= 1 << bitOff
 	return nil
 }
 
@@ -35,17 +43,17 @@ func (b *CustodyBitmap) Clear(i uint) error {
 	if i >= uint(kzg4844.CellsPerBlob) {
 		return errors.New("bit index out of range")
 	}
-	index := i / 8
-	offset := i % 8
-	b[index] &^= 1 << offset
+	byteIdx := i / 8
+	bitOff := i % 8
+	b[byteIdx] &^= 1 << bitOff
 	return nil
 }
 
 // Number of bits set to 1
 func (b CustodyBitmap) OneCount() int {
 	total := 0
-	for _, byte := range b {
-		total += bits.OnesCount8(byte)
+	for _, data := range b {
+		total += bits.OnesCount8(data)
 	}
 	return total
 }
@@ -65,7 +73,7 @@ func (b CustodyBitmap) Indices() []uint64 {
 	return out
 }
 
-func (b CustodyBitmap) SetIndices(indices []uint64) error {
+func (b *CustodyBitmap) SetIndices(indices []uint64) error {
 	for _, i := range indices {
 		if i >= uint64(kzg4844.CellsPerBlob) {
 			return errors.New("bit index out of range")
@@ -89,4 +97,59 @@ func (b CustodyBitmap) SetData() CustodyBitmap {
 		b[i] = 0xFF
 	}
 	return b
+}
+
+func (b CustodyBitmap) Same(set *CustodyBitmap) bool {
+	for i := 0; i < len(b); i++ {
+		if b[i] != set[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// Difference returns a new CustodyBitmap which is b \ set
+func (b *CustodyBitmap) Difference(set *CustodyBitmap) *CustodyBitmap {
+	var out CustodyBitmap
+	for i := 0; i < len(b); i++ {
+		out[i] = b[i] &^ set[i]
+	}
+	return &out
+}
+
+func (b *CustodyBitmap) Intersection(set *CustodyBitmap) *CustodyBitmap {
+	var out CustodyBitmap
+	for i := 0; i < len(b); i++ {
+		out[i] = b[i] & set[i]
+	}
+	return &out
+}
+
+func (b *CustodyBitmap) Union(set *CustodyBitmap) *CustodyBitmap {
+	var out CustodyBitmap
+	for i := 0; i < len(b); i++ {
+		out[i] = b[i] | set[i]
+	}
+	return &out
+}
+
+func (b CustodyBitmap) AllSet() bool {
+	totalBits := kzg4844.CellsPerBlob
+	fullBytes := totalBits / 8
+	remainBits := totalBits % 8
+
+	for i := 0; i < fullBytes; i++ {
+		if b[i] != 0xFF {
+			return false
+		}
+	}
+
+	if remainBits > 0 {
+		mask := byte((1 << remainBits) - 1)
+		if b[fullBytes]&mask != mask {
+			return false
+		}
+	}
+
+	return true
 }
